cdb00ffc4f5ad8692e81269dbb0bc2ea714053e2
HeeYeonKim98 <wow9896@naver.com>
2021-07-06 17:58:21 +0900

    :sparkles: redux-saga 설정, index 완료

---
+++ sagas/index.js
@@ -0,0 +1,8 @@
+import { all, fork } from "redux-saga/effects";
+
+import userSaga from "./user";
+import postSaga from "./post";
+
+export default function* rootSaga() {
+  yield all([fork(userSaga), fork(postSaga)]);
+}
--- store/configureStore.js
+++ store/configureStore.js
@@ -1,20 +1,24 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { createWrapper } from "next-redux-wrapper";
+import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "../reducers";
+import rootSaga from "../sagas";
 
-const ConfigureStore = () => {
-  const middlewares = [];
+const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(compose(applyMiddleware(...middlewares)));
   const store = createStore(rootReducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
-const wrapper = createWrapper(ConfigureStore, {
+const wrapper = createWrapper(configureStore, {
   debug: process.env.NODE_ENV === "development",
 });
 
